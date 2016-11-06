4a42ffb3477811ff275da6f5314acd47e3fc9346
Bill Ferster <bferster@virginia.edu>
2016-11-06 16:14:02 -0500

    radial menu 1st orbit

--- pie.js
+++ pie.js
@@ -12,23 +12,26 @@ function Pie(options)														// CONSTRUCTOR
 	var str="<div id='pimenu' class='pi-main unselectable'></div>";				// Main shell
 	$(this.ops.parent).append(str);												// Add to DOM														
 	str="<img id='piback' class='pi-slice' src='"+ops.dial+"'/>";				// Menu back			
-	str+="<img id='pihigh' class='pi-slice' style='pointer-events: none' src='philite.png'/>";	// Slice highlight				
+	str+="<img id='pihigh' class='pi-slice' style='pointer-events: none' src='img/philite.png'/>";	// Slice highlight				
 	$("#pimenu").append(str);													// Add to DOM														
-
 
 	$("#piback").on("mousemove",function(e) { 									// ON HOVER ON
  		var alpha=0,cur="auto";													// Assume off
+ 		_this.curSlice=-1;														// Assume nothing pickec
  		var w=_this.ops.wid;													// Size
  		var x=e.clientX-(_this.ops.x+_this.ops.wid/2);							// Dx from center
  		var y=e.clientY-(_this.ops.y+_this.ops.wid/2);							// Dy from center
  		var h=Math.sqrt(x*x+y*y); 												// Euclidian distance from center
-		_this.curSlice=Math.floor((180-Math.atan2(x,y)*(180/Math.PI))/45);		// Get cuerrnt slice
 		if (h < w/10) {															// In settings
-			alpha=0;   cur="pointer"											// Show it
+			alpha=0;   cur="pointer"; 											// Show it
+			_this.curSlice=0;													// Center slice
 			}
-		else if ((h > w/4) && _this.ops.active[_this.curSlice]) {				// In first orbit and an active slice
-			alpha=1;   cur="pointer"											// Show it
- 			$("#pihigh").css({"transform":"rotate("+_this.curSlice*45+"deg)"}); // Rotate highlight
+		else if (h > w/4) {														// In first orbit and an active slice
+			_this.curSlice=Math.floor((180-Math.atan2(x,y)*(180/Math.PI))/_this.ops.ang)+1;	// Get current slice
+			if ((_this.curSlice > 0) && _this.ops.active[_this.curSlice]) {		// A valid slice
+				alpha=1;   cur="pointer"										// Show it
+ 				$("#pihigh").css({"transform":"rotate("+(_this.curSlice-1)*_this.ops.ang+"deg)"}); // Rotate highlight
+				}
 			}
 		$("#pihigh").css({"opacity":alpha});									// Set highlight
 		$("#pimenu").css({"cursor":cur});										// Set cursor
@@ -41,25 +44,24 @@ function Pie(options)														// CONSTRUCTOR
 		$("#pihigh").css({"opacity":0});										// No highlight
 		$("#pimenu").css({"cursor":"auto"});									// Normal cursor
 		});	
-
+	$("#piback").on("click",function() { 										// ON CLICK
+		if (_this.curSlice >= 0)												// A valid pick
+			SendMessage("click",_this.curSlice);								// Senf event
+		});	
 }
 
-Pie.prototype.ShowPieMenu=function()										// SHOW PIE MENU
+Pie.prototype.ShowPieMenu=function(mode)									// SHOW PIE MENU
 {
 	var o=this.ops;																// Point at ops
-	$("#pimenu").css({"width":"0px","height":"0px"});							// Hide
-	$("#pimenu").css({"top":(o.y+o.wid/2)+"px","left":(o.x+o.wid/2)+"px"});		// Position
-	$("#pimenu").animate({ width:o.wid, height:o.wid,top:o.y, left:o.x });		// Zoom on
+	if (mode) {	
+		$("#pimenu").css({"width":"0px","height":"0px"});						// Hide
+		$("#pimenu").css({"top":(o.y+o.wid/2)+"px","left":(o.x+o.wid/2)+"px"});	// Position
+		$("#pimenu").animate({ width:o.wid, height:o.wid,top:o.y, left:o.x });	// Zoom on
+		}
+	else
+		$("#pimenu").animate({ width:0, height:0,top:o.y+o.wid/2,left:o.x+o.wid/2},200);	// Zoom off
 }
 
-
-
-/*
-		$("#"+id).on("click",function() {										// ON SLICE CLICK
-			$("#pimenu").remove();												// Remove menu
-			});	
-
-*/
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // HELPERS
@@ -97,3 +99,10 @@ Pie.prototype.ShowPieMenu=function()										// SHOW PIE MENU
 		return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
 		}
 
+	function SendMessage(cmd, msg, callback) 								// SEND HTML5 MESSAGE 
+	{
+		var str=cmd+"|PiMenu";													// Add src and window						
+		if (msg)																// If more to it
+			str+="|"+msg;														// Add it
+		window.parent.postMessage(str,"*");										// Send message to parent wind		
+	}
