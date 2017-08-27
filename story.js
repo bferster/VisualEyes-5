97b99b9584990afc1abea4bac421277035fccab9
Bill Ferster <bferster@virginia.edu>
2017-08-27 16:25:25 -0400

    stepped storymode

--- story.js
+++ story.js
@@ -16,36 +16,58 @@ function Story(div, pop)												// CONSTRUCTOR
 
 	this.div="#"+div;														// Current div selector
 	this.pop=pop;															// Point at popup lib
+	this.storyMode="Scrolled";												// Story mode
+	this.curPage=0;															// Current page
+	this.pages=[];															// Holds page indices
 }
-
 
 Story.prototype.InitStory=function(data)								// INIT STORY
 {
-
-/* 
-	Init library and set data
-	@param {object} data	Points to mobs
-*/
-	var i,str="",ind=0;
+	var i,k=1,str="",ind=0;
 	if (data)	this.sd=data;												// Point at data
-	var _this=this;															// Save context for callback
+	var _this=this;	
+	this.pages=[];															// Save context for callback
+	this.storyMode=this.sd.storyMode;										// Set mode
+	if (this.sd.storyMode == "Stepped") 									// If stepped
+		str+="<div style='text-align:center'>";								// Center div
 	if (this.sd.title)	str+="<div class='story-title'>"+this.sd.title+"</div>";
-	for (i=0;i<this.sd.mobs.length;++i) 									// For each mob
-		if (this.sd.mobs[i].marker && (this.sd.mobs[i].marker.toLowerCase() == "story")) { 	// If  a story item
-//			if (!dtl.ShowElement(this.sd.mobs[i]))							// If not being shown
-//				continue;													// Skip it	
-			if (this.sd.mobs[i].open == undefined) {						// If first time
-				if (this.sd.mobs[i].show && this.sd.mobs[i].show.match(/open/i))			// If set to open
-					this.sd.mobs[i].open=true;								// Set true
-				else														// Otherwise, must be closed
-					this.sd.mobs[i].open=false;								// Set false
+	if (this.sd.storyMode == "Stepped") {									// If stepped
+//		str+="<img src='img/revbut.png'>&nbsp;&nbsp;";						// Back button
+		str+="<select id='pageSel' class='ve-bs' style='vertical-align:5px;text-align:center'>"; // Selector
+		for (i=0;i<this.sd.mobs.length;++i) 								// For each mob
+			if (this.sd.mobs[i].marker && (this.sd.mobs[i].marker.toLowerCase() == "story")) { 	// If  a story item
+				str+="<option value='"+i+"'>"+(k++)+". "+this.sd.mobs[i].title+"</option>";		// Add option
+				this.pages.push(i);											// Save index to move
 				}
-			if (this.sd.mobs[i].pos)	ind=this.sd.mobs[i].pos;			// If a pos set, use it
-			str+="<div id='story"+i+"' style='margin-left:"+(ind*18+12)+"px'>"; // Container div
-			str+=this.DrawStoryItem(i)+"</div>";							// Add it to html
-			}
-	$("#rightDiv").html(str);
-}	
+		str+="</select>&nbsp;&nbsp;<img src='img/playbut.png'>";			// Forward button
+		str+="</div><br><br>";												// End center div
+		str+="<div id='storyDiv'>";											// Enclosing div										
+		str+=this.DrawStoryItem(this.pages[this.curPage])+"</div>";			// Add story to page
+		$("#rightDiv").html(str);											// Set text
+		$("#pageSel").prop("selectedIndex",this.curPage);					// Set select
+		
+		$("#pageSel").on("change", function() {								// CHANGE PAGE
+			$("#storyDiv").html(_this.DrawStoryItem($("#pageSel").val()));	// Set new page
+			_this.curPage=$("#pageSel").prop("selectedIndex");				// Get index
+			});
+		}
+	else{																	// Scrolled
+		for (i=0;i<this.sd.mobs.length;++i) 								// For each mob
+			if (this.sd.mobs[i].marker && (this.sd.mobs[i].marker.toLowerCase() == "story")) { 	// If  a story item
+				if (this.sd.mobs[i].open == undefined) {					// If first time
+					if (this.sd.mobs[i].show && this.sd.mobs[i].show.match(/open/i))			// If set to open
+						this.sd.mobs[i].open=true;							// Set true
+					else													// Otherwise, must be closed
+						this.sd.mobs[i].open=false;							// Set false
+					}
+				if (this.sd.mobs[i].pos)	ind=this.sd.mobs[i].pos;		// If a pos set, use it
+				str+="<div id='story"+i+"' style='margin-left:"+(ind*18+12)+"px'>"; // Container div
+				str+=this.DrawStoryItem(i)+"</div>";						// Add it to html
+				}
+		$("#rightDiv").html(str);											// Set text
+		}
+	}
+
 
 Story.prototype.UpdateStory=function(curTime, timeFormat) 				// UPDATE STORY PANE
 {
@@ -87,8 +109,6 @@ function onStoryClick(e,mode) 											// TOGGLE STORY ITEM
 		}
 }
 
-
-
 Story.prototype.DrawStoryItem=function(num) 							// DRAW STORY ITEM
 {
 
@@ -97,19 +117,22 @@ Story.prototype.DrawStoryItem=function(num) 							// DRAW STORY ITEM
 	@param {number} num 		Index of mob to draw
 */
 	
+	var str="";
 	var desc,col="#555",v,vv,title;
 	var mob=this.sd.mobs[num];												// Point at mob
 	if (mob.color)	col=mob.color;											// If a color set, use it
 	var _this=this;															// Save context for callback
-	var str="<div id='storyBut"+num+"' onclick='onStoryClick(this.id)' ";	// Triangle head
-	str+="style='width:0px;height:0px;display:inline-block;cursor:pointer;";	
-	if (mob.open) 															// Draw a down triangle
-		str+="border-left:6px solid transparent;border-right:6px solid transparent;border-top:10px solid #aaa'></div>";
-	else																	// Draw right triangle
-		str+="border-top:6px solid transparent;border-bottom:6px solid transparent;border-left:10px solid #aaa;margin-left:2px'></div>";
-	if (mob.title)															// If a title
-		str+="<div class='story-header' style='display:inline-block;color:"+col+"'>"+mob.title+"</div><br>";
-	if (mob.open) {															// If open
+	if (this.storyMode != "Stepped") {										// Scroll mode
+		str+="<div id='storyBut"+num+"' onclick='onStoryClick(this.id)' ";	// Triangle head
+		str+="style='width:0px;height:0px;display:inline-block;cursor:pointer;";	
+		if (mob.open) 															// Draw a down triangle
+			str+="border-left:6px solid transparent;border-right:6px solid transparent;border-top:10px solid #aaa'></div>";
+		else																	// Draw right triangle
+			str+="border-top:6px solid transparent;border-bottom:6px solid transparent;border-left:10px solid #aaa;margin-left:2px'></div>";
+		if (mob.title)															// If a title
+			str+="<div class='story-header' style='display:inline-block;color:"+col+"'>"+mob.title+"</div><br>";
+		}
+	if (mob.open || (this.storyMode == "Stepped")) {						// If open or stepped
 		if (mob.pic) {														// If a pic
 			var pic=ConvertFromGoogleDrive(mob.pic);						// Point at pic
 			str+="<img class='story-pic' style='display:inline-block;' src='"+pic+"' ";
@@ -138,7 +161,7 @@ Story.prototype.DrawStoryItem=function(num) 							// DRAW STORY ITEM
 			str+="<u>Citation</u><br><span style='display:none' id='cite"+num+"'><br>"+mob.citation+"</span></div>";
 			}
 		}
-	if (mob.open)															// If open
+	if (mob.open || (this.storyMode == "Stepped")) 							// If open or stepped
 		str+="<div style='clear:both'></div><br>";							// Clear float
 	return str;																// Return story item html
 }
