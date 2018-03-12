e61a91fe18b23d2710aa7f6f441a145f588648af
bferster <bferster@virginia.edu>
2018-03-12 09:22:31 -0400

    SSL prep

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+// configuration file for QMedia File System
+$dbname='qmedia';
+$dbuser='qmedia';
+$dbpass='';															// Supply your own (prename)
+$link=mysqli_connect("localhost", $dbuser, $dbpass, $dbname) or die ("Could not connect to server.");
+?>
